5feb864a9bb53a07bf5c7bbd1c5286c800613e99
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2021-11-05 12:24:00 +0400

    anagramm V1.2
    Упростил метод выдающий HashMap-у. Убрал итератор и оставил определение есть ли в мар-е ключ БУКВА? Если есть, то значение увеличиваем, если нет в мар-е ключа, то просто добавляем элемент.

--- Main.java
+++ Main.java
@@ -6,31 +6,21 @@ public class Main {
 // Определяем являются ли 2 слова анаграммой
     public static void main(String[] args) {
         System.out.println("Ведите 2 слова через пробел: ");
-        boolean rezalt = false;
         Scanner scanner = new Scanner(System.in);
         String word1 = scanner.next().toLowerCase();
         String word2 = scanner.next().toLowerCase();
         scanner.close();
-
-        rezalt = analyzer(word1).equals(analyzer(word2));
-
-        System.out.println(rezalt);
+        System.out.println(analyzer(word1).equals(analyzer(word2)));
     }
-
+// метод выдающий HashMap-у с количеством букв в слове
      static Map<Character, Integer> analyzer(String word) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < word.length(); i++) {
             int numChar = 1;
             char charW = word.charAt(i);
             if (!map.isEmpty()) {
-
-                Iterator<Map.Entry<Character, Integer>> iterChar = map.entrySet().iterator();
-                while (iterChar.hasNext()) {
-                    Map.Entry<Character, Integer> ent = iterChar.next();
-                    if (ent.getKey().equals(charW)) {
-                        numChar += ent.getValue();
-                        iterChar.remove();
-                    }
+                if (map.containsKey(charW)) {
+                    numChar += map.get(charW);
                 }
             }
             map.put(charW, numChar);
